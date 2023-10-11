5b962340b01acec2e5e80c367d654762af043403
Rikuto <orikutobasuke@gmail.com>
2023-10-11 23:18:27 +0900

    test,必要のない記述削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,3 @@
-import { SysContext, SysDispatchContext } from '@/context/sys-context'
-import { hideMessage } from '@/hooks/system/actioncreator'
-import { useSystemReducer } from '@/hooks/system/reducer'
 import '@/styles/globals.css'
 import { Noto_Sans_JP } from '@next/font/google'
 import type { AppProps } from 'next/app'
@@ -16,9 +13,6 @@ const notoSansJP = Noto_Sans_JP({
 })
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [sysState, sysDispatch] = useSystemReducer({
-    isLoggedin: false
-  })
 
   return (
     <>
@@ -27,18 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
           font-family: ${notoSansJP.style.fontFamily};
         }
       `}</style>
-
-      <SysContext.Provider value={sysState}>
-        <SysDispatchContext.Provider value={sysDispatch}>
-          <Component {...pageProps} />
-          <Dialog visible={sysState.isVisible}
-            header={sysState.header}
-            style={{ minWidth: '640px', minHeight: '240px' }}
-            onHide={() => hideMessage(sysDispatch)}>
-            <p>{sysState.message}</p>
-          </Dialog>
-        </SysDispatchContext.Provider>
-      </SysContext.Provider>
+      <Component {...pageProps} />
     </>
   )
 }
